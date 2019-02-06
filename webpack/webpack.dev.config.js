9f0b6832c26ff8b537baef67d7b1924e9e257e91
Poonam Sagar <poonamsagar9212@gmail.com>
2019-02-06 12:03:27 +1100

    Some changes.

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+
+module.exports = {
+	entry: [
+		path.join(__dirname, '../src/index.js')
+    ],
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loder", "less-loader"]
+            }
+        ]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    },
+    mode:'production'
+}
